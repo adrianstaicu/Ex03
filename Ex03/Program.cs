0d0d5e3119882e1c2c01863dd3806c097ded5e06
Staicu Adrian <adrianstacu888@gmail.com>
2020-01-31 00:44:02 +0200

    Add project files.

---
+++ Ex03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            if (++a < 4)
+                if (a++ < 4)
+                    Console.WriteLine(a);
+                else
+                    Console.WriteLine(a);
+
+            // Programul nu afiseaza nimic, deoarece conditia de == nu exista
+        }
+    }
+}
